10fd8c132be9338805b8f56fc65e4034bc731740
Shweta Jain <shweta@Shwetas-MacBook-Air.local>
2026-02-10 12:29:11 +0530

    Fix mobile navigation on home page

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-8 animate-fade-in md:my-16">
-        <ul className="flex items-center justify-center gap-4">
+        <ul className="flex items-center justify-center gap-8 md:gap-4">
           {navigation.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-lg duration-500 text-zinc-500 hover:text-zinc-300 md:text-sm"
             >
               {item.name}
             </Link>
